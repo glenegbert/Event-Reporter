f7fe0d06901858275d48813b7c62ebea39fd7129
Glen Egbert <glenegbert1@gmail.com>
2014-08-28 11:46:10 -0600

    cleared repo

--- test/test_helper.rb
+++
@@ -1,7 +0,0 @@
-gem 'minitest'
-require 'minitest/autorun'
-require 'minitest/pride'
-require 'pry'
-
-lib = File.expand_path('../../lib', __FILE__)
-$LOAD_PATH.unshift lib unless $LOAD_PATH.include? lib
